4af3d455a12e5750e2fcc77bb79cbacf9d3eb7cf
prgc <dalia.kuechler@stud.hslu.ch>
2021-03-11 21:25:39 +0100

    SW03

--- 21_Workshops/WS04_Schleifen_For-Loop/forLoop5.c
+++ 21_Workshops/WS04_Schleifen_For-Loop/forLoop5.c
@@ -6,11 +6,17 @@ int main(int argc, char *argv[] )
 	int start = 1;
 	int end = 100;
 	
-	for ( int i = start; i <= end; i++ )
+	for ( int i = start; i <= end; i += 10 )
 	{
+		printf("\n%d", i);
 		
-		printf("%d\t", i);
+		for( int j = i; j <= i+10; j++)
+		{
+			printf("%d", j);
+		}
+				
 	}
 	
+	printf ("\n");
 	return 0;
 }
