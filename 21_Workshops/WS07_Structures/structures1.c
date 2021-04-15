ca1a7a4ca58749109e0a7a6fad4c50350ed631ff
prgc <dalia.kuechler@stud.hslu.ch>
2021-04-15 20:26:15 +0200

    SW08

--- 21_Workshops/WS07_Structures/structures1.c
+++ 21_Workshops/WS07_Structures/structures1.c
@@ -1,51 +1,55 @@
 #include <stdio.h>
-#include <string.h>
 
 
-struct student
-{
-	char firstname[20];
-	char lastname[30];
-	float grades [6];
-	float average;
+#define MAX_NAME_LENGTH 30
+#define NUM_GRADES 6
+#define NUM_STUDENTS 5
+
+
+struct Student {
+	char firstname[MAX_NAME_LENGTH];
+	char lastname[MAX_NAME_LENGTH];
+	float grades[NUM_GRADES];
+	float average; 
 };
 
-
-int main(int argc, char *argv[] )
+int main(void)
 {
-	struct student stud[5];
-	
-	strcpy(stud[1].firstname, "Tony");
-	strcpy(stud[1].lastname, "Stark");
-	stud[1].grades = {6.0, 5.8, 5.9, 6.0, 5.9, 6.0};
-	
-	printf("Noten: %e\n", stud[1].grades);
 
-/*	
-	strcpy(stud[2].firstname, "Peter");
-	strcpy(stud[2].lastname, "Parker");
-	stud[2].grades = 4.5, 4.8, 5.8, 3.2, 5.9, 5.2;
-	
-	strcpy(stud[3].firstname, "Bruce");
-	strcpy(stud[3].lastname, "Banner");
-	stud[3].grades = 5.5, 2.8, 5.3, 4.8, 5.4, 4.5;
-	
-	strcpy(stud[4].firstname, "Steve");
-	strcpy(stud[4].lastname, "Rodgers");
-	stud[4].grades = 4.9, 3.9, 5.8, 2.3, 4.3, 5.4;
-	
-	strcpy(stud[5].firstname, "Natasha");
-	strcpy(stud[5].lastname, "Romanoff");
-	stud[5].grades = 5.3, 5.2, 5.1, 5.7, 4.9, 5.9;
-	
-*/	
-	//printf("Vorname: %s\n", s1.firstname);
+struct Student students[NUM_STUDENTS] = { 
+[0].firstname = "Tony", 	[0].lastname = "Stark", 	[0].grades = {6.0, 5.8, 5.9, 6.0, 5.9, 6.0},
+[1].firstname = "Peter", 	[1].lastname = "Parker", 	[1].grades = {4.5, 4.8, 5.8, 3.2, 5.9, 5.2},
+[2].firstname = "Bruce", 	[2].lastname = "Banner", 	[2].grades = {5.5, 2.8, 5.3, 4.8, 5.4, 4.5},
+[3].firstname = "Steve", 	[3].lastname = "Rodgers", 	[3].grades = {4.9, 3.9, 5.8, 2.3, 4.3, 5.4},
+[4].firstname = "Natasha", 	[4].lastname = "Romanoff", 	[4].grades = {5.3, 5.2, 5.1, 5.7, 4.9, 5.9} };
+
+float sum = 0;
+float summe = 0;
+float average = 0;
+float Klassendurchschnitt = 0;
+
+
+	for(int i = 0 ; i < NUM_STUDENTS; i++)
+	{
+
+		for(int j = 0; j < 6; j++)
+		{
+		sum += students[i].grades[j];
+		summe+= students[i].grades[j];
 		
-
-
-
+		}
+	average = sum / NUM_GRADES;
+	printf("Average Student %d = %.2f\n", i,  average);
+	sum = 0;
+	
+	}
+	int Anzahl = NUM_STUDENTS * NUM_GRADES;
+	Klassendurchschnitt = summe / Anzahl;
+	printf("Klassendurchschnitt = %.2f\n", Klassendurchschnitt);
+		
 
 
 
 	return 0;
 }
+
